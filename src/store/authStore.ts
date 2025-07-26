58d59972f88883f4f8f2ebdadafe216f6cc095a4
quyen-tv <tvq05112003@gmail.com>
2025-07-27 00:36:47 +0700

    feat: create auth store with Zustand to manage user state

---
+++ src/store/authStore.ts
@@ -0,0 +1,12 @@
+import type { User } from 'firebase/auth'
+import { create } from 'zustand'
+
+interface AuthState {
+  user: User | null
+  setUser: (user: User | null) => void
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user })
+}))
