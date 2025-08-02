83bca6b3da5fc8b4cfa9457fe5edf88015a58fae
quyen-tv <tvq05112003@gmail.com>
2025-08-02 16:11:52 +0700

    feat(layout): add general Layout component
    - Create a base layout component to wrap main application content.
    - This layout includes a placeholder for the Header and the main content area.
    - It will serve as a consistent wrapper for all pages.

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import type { ReactNode } from 'react'
+import { Header } from '@/components/layout/index'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className='flex h-screen bg-[var(--background)] text-[var(--foreground)]'>
+      <div className='flex-1 flex flex-col overflow-hidden'>
+        <Header />
+        <main className='flex-1 overflow-y-auto p-6'>{children}</main>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
